1a674f8b489c07bf6c5bc825b0467a99dc00abde
mattchupp <mattlchupp@gmail.com>
2020-06-19 08:14:53 -0500

    moved functions out of home()

--- app.py
+++ app.py
@@ -6,28 +6,32 @@ from flask import request
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
+# path -> / 
 @app.route('/', methods=['GET'])
 def home():
-  
-  def mock(text): 
-    newStr = []
-    output = ''
-
-    for c in text: 
-      num = randomNum()
-      if num == 1: 
-        newStr.append(c.capitalize())
-      else: 
-        newStr.append(c)
-    
-    return output.join(newStr)
-
-  def randomNum(): 
-    number = random.randint(0,1)
-    return number
   
   return mock('Hello world!')
 
 
+# Capitalize every other letter
+# ExPeCtEd OuTpUt
+def mock(text): 
+  newStr = []
+  output = ''
+
+  for c in text: 
+    num = randomNum()
+    if num == 1: 
+      newStr.append(c.capitalize())
+    else: 
+      newStr.append(c)
+  
+  return output.join(newStr)
+
+def randomNum(): 
+  number = random.randint(0,1)
+  return number
+
+
+# Run 
 app.run()
